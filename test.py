f632305f01bbf51391e535abc8c211d22c4f1f83
Bin Wang <binwang.0213@gmail.com>
2020-04-12 19:49:11 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,4 @@
 import pyHilbertSort as m
-import numpy as np
-
-assert m.__version__ == '0.0.1'
-assert m.subtract(1, 2) == -1
 
 pts_sort,idx_sort=m.hilbertSort(3, np.random.rand(5,3),True)
-print(pts_sort,idx_sort,True)
+print(pts_sort,idx_sort)
